8e27d0e0f4730d9c459965da7099be5b35162480
Sean McGee <seanamcgee@Seans-MacBook-Air.local>
2018-04-15 16:53:04 -0400

    update

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-folders = ['provided_data', 'generated_4']
+folders = ['provided_data','generated_1', 'generated_2', 'generated_3' 'generated_4']
 
 samples = []
 for folder in folders:
@@ -53,28 +53,36 @@ train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.3))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.3))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.3))
 model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Dropout(0.3))
 model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.3))
 model.add(Dense(50))
+model.add(Dropout(0.3))
 model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator,
-                    samples_per_epoch=len(train_samples)*6,
+                    steps_per_epoch=len(train_samples)/32,
+                    epochs=10,
+                    verbose=1,
                     validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples)*6,
-                    nb_epoch=5)
+                    validation_steps=len(validation_samples)/32)
 model.save('model.h5')
 exit()
