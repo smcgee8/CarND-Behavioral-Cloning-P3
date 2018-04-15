21bf87a05e664fb68ee50f1117ae16ec9670684f
Sean McGee <seanamcgee@Seans-MacBook-Air.local>
2018-04-15 17:31:11 -0400

    update

--- model.py
+++ model.py
@@ -54,20 +54,20 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(24,(5,5),strides=(2,2),activation="relu"))
 model.add(Dropout(0.3))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(36,(5,5),strides=(2,2),activation="relu"))
 model.add(Dropout(0.3))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Conv2D(48,(5,5),strides=(2,2),activation="relu"))
 model.add(Dropout(0.3))
-model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Conv2D(64,(3,3),activation="relu"))
 model.add(Dropout(0.3))
-model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Conv2D(64,(3,3),activation="relu"))
 model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(100))
